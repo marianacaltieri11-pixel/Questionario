48a461f83cd018029177ee39127f6987d5f2171e
marianacaltieri11-pixel <mariana.c.altieri.11@gmail.com>
2026-08-20 17:05:48 -0300

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,10 @@
+let totalDeAlunos = 120;
+let alunosPcd = 20;
+
+
+let percentualPcd = (alunosPcd / totalDeAlunos) * 100;
+
+
+console.log("Total de alunos: " + totalDeAlunos);
+console.log("Alunos PCD: " + alunosPcd);
+console.log("Percentual de PCD: " + percentualPcd.toFixed(2) + "%");
